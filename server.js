23247b1e1a6cae26d94c191d8f956a2a29d80a41
chebbi2508 <44964475+chebbi2508@users.noreply.github.com>
2020-12-04 10:21:50 +0100

    web service billet

--- server.js
+++ server.js
@@ -21,10 +21,12 @@ app.get('/', (req, res) => {
 // Require user routes
 const userRoutes = require('./src/routes/user.routes')
 const accessoireRoutes = require('./src/routes/accessoire.routes')
+const billetRoutes = require('./src/routes/billet.routes')
 
 // using as middleware
 app.use('/user', userRoutes)
 app.use('/accessoire', accessoireRoutes)
+app.use('/billet', billetRoutes)
 
 
 
---
+++ src/models/billet.model.js
@@ -0,0 +1,67 @@
+'user strict';
+var dbConn = require('./../../config/db.config');
+
+//user object create
+var billet = function(billet){
+    this.prix_billet     = billet.prix_billet;
+    this.matchs      = billet.matchs;
+
+};
+billet.create = function (newUsr, result) {
+    dbConn.query("INSERT INTO billet set ?", newUsr, function (err, res) {
+        if(err) {
+            console.log("error: ", err);
+            result(err, null);
+        }
+        else{
+            console.log(res.insertId);
+            result(null, res.insertId);
+        }
+    });
+};
+billet.findById = function (id, result) {
+    dbConn.query("Select * from billet where id_acc = ? ", id, function (err, res) {
+        if(err) {
+            console.log("error: ", err);
+            result(err, null);
+        }
+        else{
+            result(null, res);
+        }
+    });
+};
+billet.findAll = function (result) {
+    dbConn.query("Select * from billet", function (err, res) {
+        if(err) {
+            console.log("error: ", err);
+            result(null, err);
+        }
+        else{
+            console.log('billets : ', res);
+            result(null, res);
+        }
+    });
+};
+billet.update = function(id, billet, result){
+    dbConn.query("UPDATE billet SET prix_billet=?,matchs=? WHERE id_billet = ?", [billet.prix_billet,billet.matchs, id], function (err, res) {
+        if(err) {
+            console.log("error: ", err);
+            result(null, err);
+        }else{
+            result(null, res);
+        }
+    });
+};
+billet.delete = function(id, result){
+    dbConn.query("DELETE FROM billet WHERE id_billet = ?", [id], function (err, res) {
+        if(err) {
+            console.log("error: ", err);
+            result(null, err);
+        }
+        else{
+            result(null, res);
+        }
+    });
+};
+
+module.exports= billet;
